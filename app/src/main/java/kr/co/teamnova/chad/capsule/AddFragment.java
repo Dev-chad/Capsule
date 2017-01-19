25c09ab38f22b6d373daffca5c6129c3f31dd2ab
Chad <dev.chadlim@gmail.com>
2017-01-20 00:59:06 +0900

    ADd fragment

--- app/src/main/java/kr/co/teamnova/chad/capsule/AddFragment.java
+++ app/src/main/java/kr/co/teamnova/chad/capsule/AddFragment.java
@@ -1,12 +1,19 @@
 package kr.co.teamnova.chad.capsule;
 
+import android.Manifest;
 import android.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 /**
  * Created by Chad on 2017-01-18.
@@ -14,7 +21,10 @@ import android.widget.Button;
 
 public class AddFragment extends Fragment {
 
-    OnClickAddListener mCallback;
+    private OnClickAddListener mCallback;
+    private EditText editContentDetails;
+    private EditText editTag;
+    private ImageView imageContent;
 
     public interface OnClickAddListener{
         public void AddClickEvent();
@@ -27,16 +37,39 @@ public class AddFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add, container, false);
 
-        Button btnAdd = (Button) view.findViewById(R.id.btn_add);
-        btnAdd.setOnClickListener(new View.OnClickListener() {
+        editContentDetails = (EditText) view.findViewById(R.id.edit_content_details);
+        editTag = (EditText) view.findViewById(R.id.edit_tag);
+        imageContent = (ImageView) view.findViewById(R.id.image_content);
+        imageContent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                    requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+                } else {
+                    Intent intent = new Intent(Intent.ACTION_PICK);
+                    intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                    startActivityForResult(intent, PICK_FROM_ALBUM);
+                }
+            }
+        });
+        Button btnUpload = (Button) view.findViewById(R.id.btn_add);
+        btnUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mCallback.AddClickEvent();
             }
         });
 
+        Button btnTag = (Button) view.findViewById(R.id.btn_tag);
+        btnTag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
         return view;
     }
+
 
     @Override
     public void onAttach(Context context) {
